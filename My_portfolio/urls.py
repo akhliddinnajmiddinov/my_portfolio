1b556c73ad23d94fca18a63e8e8b6af509b9357f
Ahliddin Najmiddinov <najmiddinovahliddin04@gmail.com>
2024-09-20 16:17:23 +0500

    I added projects app

--- My_portfolio/urls.py
+++ My_portfolio/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path("main/", include("main.urls")),
     path("blogs/", include("blogs.urls")),
+    path("projects/", include("projects.urls")),
     path('admin/', admin.site.urls),
 ]

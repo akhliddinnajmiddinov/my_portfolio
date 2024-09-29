a4eb8278db9691caaff4051bf3e6939764d11076
Ahliddin Najmiddinov <najmiddinovahliddin04@gmail.com>
2024-09-29 19:10:33 +0500

    1st front-end changed!

--- My_portfolio/urls.py
+++ My_portfolio/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("main/", include("main.urls")),
+    path("", include("main.urls")),
     path("blogs/", include("blogs.urls")),
     path("projects/", include("projects.urls")),
     path('admin/', admin.site.urls),
